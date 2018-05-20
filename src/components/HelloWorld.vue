4ff9d8faed12e61b64cc4fb5f21242367f6e8e41
Joe Santoru <joesantoru@gmail.com>
2018-05-20 16:36:19 -0400

    vue-material, replaced helloworld with the codesandbox contents

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,390 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div class="page-container">
+    <md-app md-waterfall md-mode="fixed">
+      <md-app-toolbar class="md-large md-dense md-primary">
+        <div class="md-toolbar-row">
+          <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
+            <md-icon>menu</md-icon>
+          </md-button>
+          <div class="md-toolbar-section-start">
+            <span class="md-title">My Quick Budget</span>
+          </div>
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button">
+              <md-icon>more_vert</md-icon>
+            </md-button>
+          </div>
+        </div>
+        <div class="md-toolbar-row">
+          <md-tabs class="md-primary"> <!-- md-sync-route -->
+            <md-tab id="tab-home" md-label="Budget"></md-tab> <!-- to="/component"-->
+            <md-tab id="tab-pages" md-label="Reports"></md-tab>
+            <md-tab id="tab-posts" md-label="Account"></md-tab>
+            <md-tab id="tab-favorites" md-label="Setup"></md-tab>
+          </md-tabs>
+        </div>
+      </md-app-toolbar>
+
+      <md-app-drawer :md-active.sync="menuVisible" md-persistent="full">
+        <md-toolbar class="md-transparent md-dense">
+          <h5 class="md-title" style="flex: 1">Accounts</h5>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button md-dense" @click="toggleMenu">
+              <md-icon><!--keyboard_arrow_left-->account_balance</md-icon>
+            </md-button>
+          </div>
+        </md-toolbar>
+
+        <md-toolbar class="md-dense" md-elevation="0">
+          <md-icon>attach_money</md-icon>
+          <span>&nbsp;&nbsp;Cash </span>
+
+          <div class="md-toolbar-section-end">
+            <md-badge class="md-square cash-badge" md-content="$1526" style="margin-right:20px;" />
+          </div>
+        </md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <span class="md-list-item-text">&nbsp;&nbsp;M&T Checking</span>
+            <md-badge class="md-square cash-badge" md-content="$1000" />
+          </md-list-item>
+
+          <md-list-item>
+            <span class="md-list-item-text">&nbsp;&nbsp;Key Checking</span>
+            <md-badge class="md-square cash-badge" md-content="$526" />
+          </md-list-item>
+
+          <md-list-item>
+            <md-button class="md-icon-button md-dense">
+              <md-icon>add_circle</md-icon>
+            </md-button>
+            <span class="md-list-item-text">Add Cash Account</span>
+          </md-list-item>
+        </md-list>
+
+        <md-toolbar class="md-dense" md-elevation="0">
+          <md-icon>credit_card</md-icon>
+          <span>&nbsp;&nbsp;Credit Cards</span>
+          <md-badge class="md-square credit-card-badge" md-content="$625" style="margin-right:20px;" />
+          <div class="md-toolbar-section-end">
+          </div>
+        </md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <span class="md-list-item-text">&nbsp;&nbsp;Chase Sapphire</span>
+            <md-badge class="md-square credit-card-badge" md-content="$625" />
+          </md-list-item>
+
+          <md-list-item>
+            <md-button class="md-icon-button md-dense">
+              <md-icon>add_circle</md-icon>
+            </md-button>
+            <span class="md-list-item-text">Add Credit Card</span>
+          </md-list-item>
+        </md-list>
+
+      </md-app-drawer>
+
+      <md-app-content>
+
+        <h2>Budget</h2>
+        <p>Totally sweet budget subtext and options</p>
+
+        <button class="accordion" @click="collapse">
+          <span><md-icon>fastfood</md-icon>&nbsp;&nbsp;Food</span>
+          <div class="md-toolbar-section-end">
+          </div>
+        </button>
+        <div class="panel">
+          <md-table class="food-table" :class="{collapsed: isCollapsed}">
+            <md-table-row>
+              <md-table-head>Category</md-table-head>
+              <md-table-head>Budgeted</md-table-head>
+              <md-table-head>Spent</md-table-head>
+              <md-table-head>Remaining</md-table-head>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Groceries</md-table-cell>
+              <md-table-cell>$400</md-table-cell>
+              <md-table-cell>$240</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$160</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Family</md-table-cell>
+              <md-table-cell>$100</md-table-cell>
+              <md-table-cell>$185</md-table-cell>
+              <md-table-cell><span class="budget-badge over-budget-badge">-$85</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Work</md-table-cell>
+              <md-table-cell>$50</md-table-cell>
+              <md-table-cell>$30</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$20</span></md-table-cell>
+            </md-table-row>
+          </md-table>
+        </div>
+
+        <button class="accordion">
+          <span><md-icon>home</md-icon>&nbsp;&nbsp;Housing</span>
+          <div class="md-toolbar-section-end">
+          </div>
+        </button>
+        <div class="panel">
+          <md-table class="food-table">
+            <md-table-row>
+              <md-table-head>Category</md-table-head>
+              <md-table-head>Budgeted</md-table-head>
+              <md-table-head>Spent</md-table-head>
+              <md-table-head>Remaining</md-table-head>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Groceries</md-table-cell>
+              <md-table-cell>$400</md-table-cell>
+              <md-table-cell>$240</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$160</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Groceries</md-table-cell>
+              <md-table-cell>$400</md-table-cell>
+              <md-table-cell>$240</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$160</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Groceries</md-table-cell>
+              <md-table-cell>$400</md-table-cell>
+              <md-table-cell>$240</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$160</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Family</md-table-cell>
+              <md-table-cell>$100</md-table-cell>
+              <md-table-cell>$185</md-table-cell>
+              <md-table-cell><span class="budget-badge over-budget-badge">-$85</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Work</md-table-cell>
+              <md-table-cell>$50</md-table-cell>
+              <md-table-cell>$30</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$20</span></md-table-cell>
+            </md-table-row>
+          </md-table>
+        </div>
+
+
+        <button class="accordion">
+          <span><md-icon>drive_eta</md-icon>&nbsp;&nbsp;Transportation</span>
+          <div class="md-toolbar-section-end">
+          </div>
+        </button>
+        <div class="panel">
+          <md-table class="food-table">
+            <md-table-row>
+              <md-table-head>Category</md-table-head>
+              <md-table-head>Budgeted</md-table-head>
+              <md-table-head>Spent</md-table-head>
+              <md-table-head>Remaining</md-table-head>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Groceries</md-table-cell>
+              <md-table-cell>$400</md-table-cell>
+              <md-table-cell>$240</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$160</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Family</md-table-cell>
+              <md-table-cell>$100</md-table-cell>
+              <md-table-cell>$185</md-table-cell>
+              <md-table-cell><span class="budget-badge over-budget-badge">-$85</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Work</md-table-cell>
+              <md-table-cell>$50</md-table-cell>
+              <md-table-cell>$30</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$20</span></md-table-cell>
+            </md-table-row>
+          </md-table>
+        </div>
+
+        <button class="accordion">
+          <span><md-icon>drive_eta</md-icon>&nbsp;&nbsp;Transportation</span>
+          <div class="md-toolbar-section-end">
+          </div>
+        </button>
+        <div class="panel">
+          <md-table class="food-table">
+            <md-table-row>
+              <md-table-head>Category</md-table-head>
+              <md-table-head>Budgeted</md-table-head>
+              <md-table-head>Spent</md-table-head>
+              <md-table-head>Remaining</md-table-head>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Groceries</md-table-cell>
+              <md-table-cell>$400</md-table-cell>
+              <md-table-cell>$240</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$160</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Family</md-table-cell>
+              <md-table-cell>$100</md-table-cell>
+              <md-table-cell>$185</md-table-cell>
+              <md-table-cell><span class="budget-badge over-budget-badge">-$85</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Work</md-table-cell>
+              <md-table-cell>$50</md-table-cell>
+              <md-table-cell>$30</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$20</span></md-table-cell>
+            </md-table-row>
+          </md-table>
+        </div>
+
+        <button class="accordion">
+          <span><md-icon>drive_eta</md-icon>&nbsp;&nbsp;Transportation</span>
+          <div class="md-toolbar-section-end">
+          </div>
+        </button>
+        <div class="panel">
+          <md-table class="food-table">
+            <md-table-row>
+              <md-table-head>Category</md-table-head>
+              <md-table-head>Budgeted</md-table-head>
+              <md-table-head>Spent</md-table-head>
+              <md-table-head>Remaining</md-table-head>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Groceries</md-table-cell>
+              <md-table-cell>$400</md-table-cell>
+              <md-table-cell>$240</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$160</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Family</md-table-cell>
+              <md-table-cell>$100</md-table-cell>
+              <md-table-cell>$185</md-table-cell>
+              <md-table-cell><span class="budget-badge over-budget-badge">-$85</span></md-table-cell>
+            </md-table-row>
+
+            <md-table-row>
+              <md-table-cell>Eating Out - Work</md-table-cell>
+              <md-table-cell>$50</md-table-cell>
+              <md-table-cell>$30</md-table-cell>
+              <md-table-cell><span class="budget-badge under-budget-badge">$20</span></md-table-cell>
+            </md-table-row>
+          </md-table>
+        </div>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .md-app {
+    height: 100vh;
+    /*border: 1px solid rgba(#000, 0.12);*/
+  }
+
+  .md-drawer {
+    width: 230px;
+    /*max-width: calc(100vw - 125px);*/
+  }
+
+  .over-budget-badge {
+    background-color: red;
+  }
+
+  .under-budget-badge {
+    background-color: green;
+  }
+
+  .budget-badge {
+    color: white;
+    padding: 4px;
+    border-radius: 5px;
+  }
+
+  .cash-badge {
+    background-color: green;
+  }
+
+  .credit-card-badge {
+    background-color: red;
+  }
+
+  /* accordian styling */
+  .category-title {
+    margin: 0;
+    font-weight: normal;
+  }
+
+  .collapsed {
+    height: 0px;
+    overflow: hidden;
+    transition: height 0.25s ease-in;
+  }
+
+  .accordion {
+    background-color: #eee;
+    color: #444;
+    cursor: pointer;
+    width: 100%;
+    border: none;
+    text-align: left;
+    outline: none;
+    font-size: 15px;
+    transition: 0.4s;
+    padding: 10px;
+  }
+
+  .active,
+  .accordion:hover {
+    background-color: #ccc;
+  }
+
+  .panel {
+    padding: 0 18px;
+    background-color: white;
+    max-height: auto;
+    transition: max-height 0.2s ease-out;
+    /*max-height: 0;*/
+    /*overflow: hidden;*/
+  }
 </style>
+
+<script>
+  export default {
+    name: "HelloWorld",
+    data: () => ({
+      menuVisible: true,
+      isCollapsed: false
+    }),
+    methods: {
+      toggleMenu() {
+        this.menuVisible = !this.menuVisible;
+      },
+      collapse() {
+        console.log("collapse");
+        this.isCollapsed = !this.isCollapsed;
+      }
+    }
+  };
+</script>
