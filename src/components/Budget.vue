18bc26c937c63b796d69f5d70306161b2efb839f
jsantoru <joesantoru@gmail.com>
2018-05-22 23:49:21 -0400

    Header Table (#2)
    * fixed border, forced badges green
    
    * brought in vuetify, initial body with expansion panels
    
    * custom badge
    
    * router!
    
    * budget component
    
    * separated router to its own file
    
    * drawer component
    
    * styling, alignment
    
    * row height, align header center
    
    * renamed HelloWorld to PageContainer
    
    * initial dynamic rendering, Vue2Filters for currency filter
    
    * display category group values
    
    * added icon support in category group header
    
    * swapped drawer icons
    
    * adjusted column widths and padding
    
    * added placeholder components
    
    * remove outer disabled scrollbar, stretch content entire width
    
    * added top border for header row
    
    * custom currency input component using md-input
    
    * display prev formatted value on focus
    
    * spent category using new input component
    
    * hide outer scrollbar (again). added overflow hidden directly to html style
    
    * adjusted font sizing
    
    * added border around category group header
    
    * added TODO and commented out label on input box
    
    * override the input font size by using refs
    
    * custom badges
    
    * added summary table, other material icons, reference font awesome but not currently using
    
    * added flexbox to header, calculate days remaining

--- src/components/Budget.vue
+++ src/components/Budget.vue
@@ -1,9 +1,33 @@
 <template>
   <div class="budget-container">
     <div class="heading">
-      <h2>Budget</h2>
-      <p>Totally sweet budget subtext and options</p>
+      <div class="heading-left">
+        <h2 style="text-align:center;padding-bottom:5px;">May 2018</h2>
+      </div>
+      <div class="heading-center">
+
+        <div class="mat-table">
+          <div class="mat-header-row summary-header">
+            <div class="mat-header-cell currency-col">BUDGETED</div>
+            <div class="mat-header-cell currency-col">SPENT</div>
+            <div class="mat-header-cell currency-col">BUDGET REMAINING</div>
+            <div class="mat-header-cell currency-col">DAYS REMAINING</div>
+
+          </div>
+          <div class="mat-row">
+            <div class="mat-cell currency-col">{{getTotalBudgeted() | currency}}</div>
+            <div class="mat-cell currency-col">{{getTotalSpent() | currency}}</div>
+            <div class="mat-cell currency-col">{{(Math.trunc(100 - (getTotalSpent() / getTotalBudgeted()) * 100))}}%</div>
+            <div class="mat-cell currency-col">{{getMonthDaysLeft()}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="heading-right">
+
+      </div>
     </div>
+
+    <!-- begin main table -->
     <div class="mat-table">
       <div class="mat-header-row">
         <div class="mat-header-cell category-col">CATEGORY</div>
@@ -14,7 +38,7 @@
     </div>
 
     <!-- dynamic -->
-    <v-expansion-panel v-for="categoryGroup in budget.categoryGroups" >
+    <v-expansion-panel v-for="categoryGroup in budget.categoryGroups" class="border-top">
       <v-expansion-panel-content :value="true" class="grey lighten-3">
         <div slot="header">
           <div class="category-group-row">
@@ -24,15 +48,25 @@
               <span class="category-group-name">{{categoryGroup.name}}</span></div>
             <div class="mat-cell currency-col">{{getCategoryGroupBudgeted(categoryGroup) | currency}}</div>
             <div class="mat-cell currency-col">{{getCategoryGroupSpent(categoryGroup) | currency}}</div>
-            <div class="mat-cell currency-col last-col">{{getCategoryGroupRemaining(categoryGroup) | currency}}</div>
-
+            <div class="mat-cell currency-col last-col">
+              <span class="category-group-remaining-cell">{{getCategoryGroupRemaining(categoryGroup) | currency}}</span>
+            </div>
           </div>
         </div>
         <div v-for="category in categoryGroup.categories" class="mat-row">
           <div class="mat-cell category-col">{{category.name}}</div>
-          <div class="mat-cell currency-col">{{category.budgeted | currency}}</div>
-          <div class="mat-cell currency-col">{{category.spent | currency}}</div>
-          <div class="mat-cell currency-col last-col">{{(category.budgeted - category.spent) | currency}}</div>
+          <div class="mat-cell currency-col">
+            <currency-input v-model="category.budgeted" @focus.native="$event.target.select();"></currency-input>
+          </div>
+          <div class="mat-cell currency-col">
+            <currency-input v-model="category.spent"></currency-input>
+          </div>
+          <div class="mat-cell currency-col last-col">
+            <span class="budget-badge"
+                :class="{'under-budget-badge': getCategoryRemaining(category) > 0, 'over-budget-badge': getCategoryRemaining(category) < 0, 'on-budget-badge': getCategoryRemaining(category) === 0}">
+              <span class="budget-badge-value">{{getCategoryRemaining(category) | currency}}</span>
+            </span>
+          </div>
         </div>
       </v-expansion-panel-content>
     </v-expansion-panel>
@@ -41,33 +75,57 @@
 
 <style scoped>
   .heading {
-    text-align: center;
+    box-shadow: 0 3px 2px -2px gray;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .heading-center {
+    flex: 2;
+  }
+
+  .heading-left,
+  .heading-right {
+    flex: 1;
+  }
+
+  .summary-header {
+    background-color: whitesmoke;
   }
 
   .category-group-name {
     padding-left: 5px;
   }
 
+  /* badge */
   .over-budget-badge {
-    background-color: red;
+    background-color: darkred;
   }
 
   .under-budget-badge {
     background-color: green;
   }
 
+  .on-budget-badge {
+    background-color: darkgray;
+  }
+
   .budget-badge {
+    border-radius: 20px;
+    height: 25px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .budget-badge-value {
     color: white;
-    padding: 4px;
-    border-radius: 10px;
-  }
-
-  .cash-badge {
-    background-color: green;
-  }
-
-  .credit-card-badge {
-    background-color: red;
+    padding-left:8px;
+    padding-right:8px;
   }
 
   /* material table styling */
@@ -78,9 +136,7 @@
   .mat-row,
   .mat-header-row {
     display: flex;
-    border-bottom-width: 1px;
-    border-bottom-style: solid;
-    border-bottom-color: lightgray;
+    border-top: 1px solid lightgray;
     align-items: center;
     min-height: 34px;
     padding: 0 24px;
@@ -103,10 +159,13 @@
     display: flex;
   }
 
-  .mat-row,
-  .mat-header-row,
-  .category-group-row {
+  .mat-header-row {
     font-size: 12px;
+  }
+
+  .mat-row,
+  .category-group-row {
+    font-size: 14px;
   }
 
   .mat-cell,
@@ -114,7 +173,6 @@
     flex: 1;
     overflow: hidden;
     word-wrap: normal;
-    /*border:1px solid black;*/
   }
 
   .category-col {
@@ -122,7 +180,6 @@
   }
 
   .currency-col {
-    /*border: 1px solid black;*/
     display: flex;
     justify-content: flex-end;
   }
@@ -131,8 +188,13 @@
     padding-right:20px;
   }
 
-  .expansion-panel__header {
-    background-color: whitesmoke;
+  .category-group-remaining-cell {
+    /* additional 8px for the badge padding so the group remaining amount stays aligned */
+    padding-right: 8px;
+  }
+
+  .border-top {
+    border-top: 1px solid lightgray;
   }
 
   /** TODO: hide */
@@ -141,13 +203,39 @@
 
 <script>
   import VExpansionPanel from "vuetify/src/components/VExpansionPanel/VExpansionPanel";
+  import VueNumeric from 'vue-numeric';
+  import CurrencyInput from './CurrencyInput.vue';
+
   export default {
-    components: {VExpansionPanel},
+    components: {VExpansionPanel, VueNumeric, CurrencyInput},
     name: "Budget",
     data: () => ({
-      budget: ""
+      budget: "",
+      labelText:""
     }),
     methods: {
+      getMonthDaysLeft(){
+        const date = new Date();
+        return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate() - date.getDate();
+      },
+      getTotalBudgeted() {
+        let total = 0;
+        for (const categoryGroup of this.budget.categoryGroups) {
+          for (const category of categoryGroup.categories) {
+            total += category.budgeted;
+          }
+        }
+        return total;
+      },
+      getTotalSpent() {
+        let total = 0;
+        for (const categoryGroup of this.budget.categoryGroups) {
+          for (const category of categoryGroup.categories) {
+            total += category.spent;
+          }
+        }
+        return total;
+      },
       getCategoryGroupBudgeted(categoryGroup) {
         let total = 0;
         for (const category of categoryGroup.categories) {
@@ -165,6 +253,9 @@
       getCategoryGroupRemaining(categoryGroup) {
         return this.getCategoryGroupBudgeted(categoryGroup) - this.getCategoryGroupSpent(categoryGroup);
       },
+      getCategoryRemaining(category) {
+        return category.budgeted - category.spent;
+      },
       retrieveBudget() {
         console.log("retrieve");
 
@@ -173,22 +264,24 @@
             {
               name: "Food",
               mdicon: "fastfood",
+              faicon: "",
               categories: [
-                {name: "Groceries", budgeted: 400, spent: 327.37},
+                {name: "Groceries", budgeted: 300, spent: 327.37},
                 {name: "Family Eating Out", budgeted: 175, spent: 158.99},
                 {name: "Work Eating Out", budgeted: 80, spent: 77.47},
               ]
             },
             {
               name: "Housing",
+              mdicon: "home",
               categories: [
-                {name: "Mortgage", budgeted: 1214.80, spent: 1214.80},
+                {name: "Mortgage", budgeted: 1510.80, spent: 1510.80},
                 {name: "Additional Mortgage", budgeted: 0, spent: 0},
-                {name: "Work Eating Out", budgeted: 80, spent: 77.47},
               ]
             },
             {
               name: "Cars",
+              mdicon: "directions_car",
               categories: [
                 {name: "Car Loan", budgeted: 307.55, spent: 0},
                 {name: "Gas", budgeted: 125, spent: 95.42},
@@ -198,6 +291,7 @@
             },
             {
               name: "Bills",
+              mdicon: "drafts",
               categories: [
                 {name: "Electric", budgeted: 200, spent: 182.61},
                 {name: "Cellphone", budgeted: 60, spent: 53.36},
@@ -205,7 +299,8 @@
               ]
             },
             {
-              name: "Subscriptions/Memberships",
+              name: "Subscriptions",
+              mdicon: "360",
               categories: [
                 {name: "YouTube TV", budgeted: 40, spent: 40},
                 {name: "Spotify", budgeted: 10.98, spent: 10.98},
---
+++ src/components/CurrencyInput.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <md-field>
+      <!-- TODO: investigate a good use for this field (maybe the ideal budgeted value? how much remaining? how much overspent by?) -->
+      <!--<label v-if="prevValue">{{prevValue}}</label>-->
+    <md-input ref="input"
+        v-model="displayValue"
+        placeholder="Enter Value"
+        @blur="isInputActive = false; prevValue = null"
+        @focus="isInputActive = true; prevValue = getPreviousDisplayValue()"
+        @keyup.native.enter="onEnter()">
+        <!-- @focus.native="$event.target.select();"> selectAll - works, but then is immediately not selected because the value changes-->
+    </md-input>
+    </md-field>
+  </div>
+</template>
+
+<style scoped>
+  input {
+    text-align: right;
+    max-width: 150px;
+
+    /* can't seem to override font-size */
+    font-size: 12px;
+  }
+
+  /* remove bottom line when not being edited */
+  .md-field:after {
+    height: 0px;
+  }
+
+  /* override the material padding */
+  .md-field {
+    padding: 0;
+    margin: 0;
+    min-height: 0;
+  }
+
+</style>
+
+<script>
+  export default {
+    components: {},
+    name: "CurrencyInput",
+    props: ["value"],
+    data: function () {
+      return {
+        prevValue: "",
+        isInputActive: false
+      }
+    },
+    computed: {
+      displayValue: {
+        get: function () {
+          if (this.isInputActive) {
+            // Cursor is inside the input field. unformat display value for user
+            return this.value.toString()
+          } else {
+            // User is not modifying now. Format display value for user interface
+            return "$ " + this.value.toFixed(2).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,")
+          }
+        },
+
+        // TODO: define a WIP value and then send event to update value only on "ENTER" or "blur"
+        // TODO: do not send an update on "ESC"
+        set: function (modifiedValue) {
+          // Recalculate value after ignoring "$" and "," in user input
+          let newValue = parseFloat(modifiedValue.replace(/[^\d\.]/g, ""))
+          // Ensure that it is not NaN
+          if (isNaN(newValue)) {
+            newValue = 0
+          }
+          // Note: we cannot set this.value as it is a "prop". It needs to be passed to parent component
+          // $emit the event so that parent component gets it
+
+          this.$emit('input', newValue)
+        }
+      }
+    },
+    methods: {
+      onEnter() {
+        console.log("ENTER");
+        this.isInputActive = false;
+
+        // TODO: remove focus
+        // TODO: focus on something else??
+
+      },
+
+      getPreviousDisplayValue() {
+        return "$ " + this.value.toFixed(2).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, "$1,");
+
+      }
+    },
+
+    mounted: function() {
+      // override the vue-material font-size for inputs
+      // overriding with classes in the template didn't work, so override it after it's been mounted
+      this.$refs.input.$el.style.fontSize = "14px";
+    }
+  }
+</script>
